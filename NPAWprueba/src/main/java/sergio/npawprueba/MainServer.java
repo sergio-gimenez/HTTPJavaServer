e3a35d47a3ace7e2752d3b4d6dfac75c20914ff4
sergio-gimenez <sergiogimenezanton@gmail.com>
2019-06-12 21:20:33 +0200

    Hecho un simple server multithread

---
+++ NPAWprueba/src/main/java/sergio/npawprueba/MainServer.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sergio.npawprueba;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+/**
+ *
+ * @author sergio
+ */
+public class MainServer {
+
+    private static final int PORT_NUMBER = 5000;
+    private static ServerSocket serverSocket;
+    private static ClientHandler clientHandler;
+    private static Thread thread;
+
+    public static void main(String[] args) throws IOException {
+        serverSocket = new ServerSocket(PORT_NUMBER);
+
+        while (true) {
+            clientHandler = new ClientHandler(serverSocket.accept());
+            new Thread(clientHandler).start();
+        }
+    }
+
+    protected void finallize() throws IOException {
+        serverSocket.close();
+    }
+}
