7f675d56ea00e1e5a9ff1d9618af923d468dea93
sergio-gimenez <sergiogimenezanton@gmail.com>
2019-06-12 20:55:24 +0200

    Single Threaded Server

---
+++ NPAWprueba/src/main/java/sergio/npawprueba/SingleThreadServer.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sergio.npawprueba;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author sergio
+ */
+public class SingleThreadServer {
+
+    public static void main(String[] args) {
+        serverConnection();
+    }
+
+    public static void serverConnection() {
+        try {
+            //Try connect to the server on an unused port eg 9991. A successful connection will return a socket
+            ServerSocket serverSocket = new ServerSocket(9991);
+            Socket connectedSocket = serverSocket.accept();
+
+            //Create Input&Outputstreams for the connection
+            InputStream inputToServer = connectedSocket.getInputStream();
+            OutputStream outputFromServer = connectedSocket.getOutputStream();
+
+            Scanner scanner = new Scanner(inputToServer, "UTF-8");
+            PrintWriter serverPrintOut = new PrintWriter(new OutputStreamWriter(outputFromServer, "UTF-8"), true);
+
+            serverPrintOut.println("Hello World! press any key to exit.");
+
+        } catch (IOException ex) {
+            Logger.getLogger(SingleThreadServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
