8c414ed8eb60233d1a3dfe45350c1d1cf3af3968
sergio-gimenez <sergiogimenezanton@gmail.com>
2019-06-14 17:45:49 +0200

    Version final que ya cumple con los requisitos

---
+++ sergioProva/build/classes/sql/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE serviceConfig (accountCode TEXT, targetDevice TEXT, pluginVersion TEXT, pingTime INTEGER, hostA INTEGER, hostB INTEGER);
+INSERT INTO serviceConfig (accountCode,targetDevice,pluginVersion,pingTime,hostA,hostB) VALUES ('clienteA','XBox','3.3.1',10,70,30);
+INSERT INTO serviceConfig (accountCode,targetDevice,pluginVersion,pingTime,hostA,hostB) VALUES ('clienteA','Panasonic','3.3.2',5,0,100);
+INSERT INTO serviceConfig (accountCode,targetDevice,pluginVersion,pingTime,hostA,hostB) VALUES ('clienteB','osmf','3.3.1',5,50,50);
---
+++ sergioProva/src/sergioprova/SelectApp.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sergioprova;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author sqlitetutorial.net
+ */
+public class SelectApp {
+
+    /**
+     * Connect to the test.db database
+     *
+     * @return the Connection object
+     */
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:/home/sergio/Escritorio/sergioProva/src/sql/db.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public String getResponseFromDB(String accountCode, String targetDevice, String pluginVersion) {
+        String sql = "SELECT pingTime, hostA, hostB FROM serviceConfig"
+                + " WHERE accountCode = ? AND targetDevice = ? AND "
+                + "pluginVersion = ? ";
+
+        try (Connection conn = this.connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            // set the value            
+            pstmt.setString(1, accountCode);
+            pstmt.setString(2, targetDevice);
+            pstmt.setString(3, pluginVersion);
+
+            //
+            ResultSet rs = pstmt.executeQuery();
+
+            // loop through the result set
+            String dbResponse = null;
+            while (rs.next()) {
+                dbResponse = (rs.getInt("pingTime") + ","
+                        + rs.getInt("hostA") + ","
+                        + rs.getInt("hostB"));
+            }
+
+            return dbResponse;
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SelectApp app = new SelectApp();
+        app.getResponseFromDB("clienteA", "XBox", "3.3.1");
+    }
+}
